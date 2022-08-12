64c57d6379603df6051ce15c34ef98b0668dcbc2
Garrett Charles <garrettguycharles@gmail.com>
2022-08-12 14:31:28 -0600

    Export stars

--- index.js
+++ index.js
@@ -10,37 +10,27 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.XmlTools = exports.PathUtils = exports.functions = exports.FileReader = exports.Tuple = exports.Stack = exports.Set = exports.Queue = exports.Observable = exports.LRUCache = exports.LinkedList = exports.RsaService = exports.CryptoService = exports.AesService = exports.Logger = exports.ApiLogger = exports.Input = exports.Semaphore = exports.Tickable = void 0;
-exports.Tickable = __importStar(require("./src/engines/Tickable"));
-exports.Semaphore = __importStar(require("./src/engines/Semaphore"));
-exports.Input = __importStar(require("./src/io/Input"));
-exports.ApiLogger = __importStar(require("./src/logging/ApiLogger"));
-exports.Logger = __importStar(require("./src/logging/Logger"));
-exports.AesService = __importStar(require("./src/services/AesService"));
-exports.CryptoService = __importStar(require("./src/services/CryptoService"));
-exports.RsaService = __importStar(require("./src/services/RsaService"));
-exports.LinkedList = __importStar(require("./src/structures/LinkedList"));
-exports.LRUCache = __importStar(require("./src/structures/LRUCache"));
-exports.Observable = __importStar(require("./src/structures/Observable"));
-exports.Queue = __importStar(require("./src/structures/Queue"));
-exports.Set = __importStar(require("./src/structures/Set"));
-exports.Stack = __importStar(require("./src/structures/Stack"));
-exports.Tuple = __importStar(require("./src/structures/Tuple"));
-exports.FileReader = __importStar(require("./src/utils/FileReader"));
-exports.functions = __importStar(require("./src/utils/functions"));
-exports.PathUtils = __importStar(require("./src/utils/PathUtils"));
-exports.XmlTools = __importStar(require("./src/utils/XmlTools"));
+__exportStar(require("./src/engines/Tickable"), exports);
+__exportStar(require("./src/engines/Semaphore"), exports);
+__exportStar(require("./src/io/Input"), exports);
+__exportStar(require("./src/logging/ApiLogger"), exports);
+__exportStar(require("./src/logging/Logger"), exports);
+__exportStar(require("./src/services/AesService"), exports);
+__exportStar(require("./src/services/CryptoService"), exports);
+__exportStar(require("./src/services/RsaService"), exports);
+__exportStar(require("./src/structures/LinkedList"), exports);
+__exportStar(require("./src/structures/LRUCache"), exports);
+__exportStar(require("./src/structures/Observable"), exports);
+__exportStar(require("./src/structures/Queue"), exports);
+__exportStar(require("./src/structures/Set"), exports);
+__exportStar(require("./src/structures/Stack"), exports);
+__exportStar(require("./src/structures/Tuple"), exports);
+__exportStar(require("./src/utils/FileReader"), exports);
+__exportStar(require("./src/utils/functions"), exports);
+__exportStar(require("./src/utils/PathUtils"), exports);
+__exportStar(require("./src/utils/XmlTools"), exports);
 //# sourceMappingURL=index.js.map
