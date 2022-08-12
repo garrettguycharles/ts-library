621030d76f937256471086d2bdd2071097789eaa
Garrett Charles <garrettguycharles@gmail.com>
2022-08-12 14:27:47 -0600

    Try this

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,11 @@
-
-// expose all classes
-
-// engines
 export * as Tickable from './src/engines/Tickable';
 export * as Semaphore from './src/engines/Semaphore';
-
-// io
 export * as Input from './src/io/Input';
-
-// logging
 export * as ApiLogger from './src/logging/ApiLogger';
 export * as Logger from './src/logging/Logger';
-
-// services
 export * as AesService from './src/services/AesService';
 export * as CryptoService from './src/services/CryptoService';
 export * as RsaService from './src/services/RsaService';
-
-// structures
 export * as LinkedList from './src/structures/LinkedList';
 export * as LRUCache from './src/structures/LRUCache';
 export * as Observable from './src/structures/Observable';
@@ -25,10 +13,8 @@ export * as Queue from './src/structures/Queue';
 export * as Set from './src/structures/Set';
 export * as Stack from './src/structures/Stack';
 export * as Tuple from './src/structures/Tuple';
-
-// utils
 export * as FileReader from './src/utils/FileReader';
 export * as functions from './src/utils/functions';
 export * as PathUtils from './src/utils/PathUtils';
 export * as XmlTools from './src/utils/XmlTools';
-
+//# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-module.exports = import * from 'dist';
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.XmlTools = exports.PathUtils = exports.functions = exports.FileReader = exports.Tuple = exports.Stack = exports.Set = exports.Queue = exports.Observable = exports.LRUCache = exports.LinkedList = exports.RsaService = exports.CryptoService = exports.AesService = exports.Logger = exports.ApiLogger = exports.Input = exports.Semaphore = exports.Tickable = void 0;
+exports.Tickable = __importStar(require("./src/engines/Tickable"));
+exports.Semaphore = __importStar(require("./src/engines/Semaphore"));
+exports.Input = __importStar(require("./src/io/Input"));
+exports.ApiLogger = __importStar(require("./src/logging/ApiLogger"));
+exports.Logger = __importStar(require("./src/logging/Logger"));
+exports.AesService = __importStar(require("./src/services/AesService"));
+exports.CryptoService = __importStar(require("./src/services/CryptoService"));
+exports.RsaService = __importStar(require("./src/services/RsaService"));
+exports.LinkedList = __importStar(require("./src/structures/LinkedList"));
+exports.LRUCache = __importStar(require("./src/structures/LRUCache"));
+exports.Observable = __importStar(require("./src/structures/Observable"));
+exports.Queue = __importStar(require("./src/structures/Queue"));
+exports.Set = __importStar(require("./src/structures/Set"));
+exports.Stack = __importStar(require("./src/structures/Stack"));
+exports.Tuple = __importStar(require("./src/structures/Tuple"));
+exports.FileReader = __importStar(require("./src/utils/FileReader"));
+exports.functions = __importStar(require("./src/utils/functions"));
+exports.PathUtils = __importStar(require("./src/utils/PathUtils"));
+exports.XmlTools = __importStar(require("./src/utils/XmlTools"));
+//# sourceMappingURL=index.js.map
